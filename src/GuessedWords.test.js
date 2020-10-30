99b06ef9e555ff062ded23405b2542442aaa564c
mikeislearning <tothemac@gmail.com>
2020-10-30 18:13:08 -0400

    ejected - testing for guessed words

---
+++ src/GuessedWords.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { shallow } from 'Enzyme';
+import { findByTestAttr, checkProps} from '../test/testUtils';
+import GuessedWords from './GuessedWords';
+
+const defaultProps = {
+  guessedWords: [
+    { guessedWord: 'train', letterMatchCount: 3 },
+  ],
+};
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords { ...setupProps} />);
+}
+
+
+test('does not throw warning with expected props', () => {
+  checkProps(GuessedWords, defaultProps);
+})
+
+describe ('if there are no words guessed', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  })
+
+  test('renders without error', () => {
+    // const wrapper = setup({ guessedWords: []});
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+
+    expect(component.length).toBe(1);
+  })
+
+  test('renders instructions to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions');
+    expect(instructions.text().length).not.toBe(0);
+    // const setup = wrapper();
+  })
+
+});
+
+describe ('if there are words guessed', () => {
+
+});
