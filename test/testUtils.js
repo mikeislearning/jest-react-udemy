48ec5dfc59efa4ad768c5d7686f723d537f483dd
mikeislearning <tothemac@gmail.com>
2020-11-01 17:38:32 -0500

    completed section 5 of udemy course

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,16 @@
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
+
+import rootReducer from '../src/reducers';
+
+/**
+ * storeFactory.
+ *
+ * @param {} initialState
+ */
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+};
 /**
  * findbyTestAttr.
  *
